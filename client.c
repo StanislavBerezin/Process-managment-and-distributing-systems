20a25a07f4c098902064b6b1a5b86d36cb88461e
Eric Lee <Raidenlkl@gmail.com>
2018-10-16 22:38:32 +1000

    Simple socket connection finished, will improve ASAP

--- client.c
+++ client.c
@@ -6,8 +6,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+//Sokcet Header file
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 #define NAME_LENGTH 7
 #define PASS_LENGTH 6
+//Connection
+#define PORT_NUMBER 9002
 
 char username[NAME_LENGTH];
 char password[PASS_LENGTH];
@@ -21,6 +28,7 @@ void connectToServer();
 int main(int argc, char *argv[])
 {
     welcomeToGameScreen();
+    connectToServer();
     return 1;
 }
 // need to send details to server from char username and password
@@ -30,6 +38,43 @@ void authentate()
 // connect to server
 void connectToServer()
 {
+    // Notes: Simple demo for testing Connection, will change later on
+
+    // Create a socket
+    int network_socket;
+    // First Parameter is the domain of the socket -> Becuase it is internet socket so it is AF_INET
+    // Second Parameter SOCK_STEAM = Using TCP
+    // thrid Parameter is define the protocol we use 0 for TCP
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Specify an address for the socket
+    struct sockaddr_in server_address;
+    // Becuase it is internet socket so it is AF_INET
+    server_address.sin_family = AF_INET;
+    // Using 9002 Por
+    server_address.sin_port = htons(9002);
+    // Connect to 0.0.0.0 IP
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // First Parameter is the actual socket
+    int connection_status = connect(network_socket, (struct sockaddr*) &server_address, sizeof(server_address) );
+
+    // Check for error with the connection
+    if (connection_status == -1){
+        printf("There was an error making a connection to the remote socket \n\n\n");
+    }
+
+    // Recieve data from the server
+    char server_response[256];
+    // Socket / Pointer to the recieve data / size for the response / FLAG
+    recv(network_socket, &server_response, sizeof(server_response) , 0 );
+
+    // Print out the server 's response
+    printf(" %s \n", server_response);
+
+    // and then close the socket
+    close(network_socket);
+
 }
 
 // Function definitions
--- server.c
+++ server.c
@@ -9,12 +9,20 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+//Sokcet Header file
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 #define AUTH_TXT "Authentication.txt"
 
 #define RANDOM_NUMBER_SEED 42
 #define MINES_NUMBER 10
 #define NUM_TILES_X 9
 #define NUM_TILES_Y 9
+
+//Connection
+#define PORT_NUMBER 9002
 
 // functions declarations
 void init();
@@ -25,11 +33,48 @@ void exitGame();
 int main(int argc, char *argv[])
 {
     signal(SIGINT, exitGame);
-
+    SendWelcomeMsg();
     return 1;
 }
 
 // Function definitions
+// Send Message back to Client
+void SendWelcomeMsg(){
+    // Notes: It is just a testing function, will change later on
+
+    // create the server socket
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //define the server address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    // basically will result to any IP address on the local machine
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // bind the socket to our specified IP and port 
+    bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+    printf("start listening to the client!!! \n");
+    // *** Important *** second argument is a backlog which is 
+    // how many connection can be waiting for this particular socket at one point in time
+    listen(server_socket, 5);
+    
+    // Get the socket
+    int client_socket;
+    client_socket = accept(server_socket, NULL, NULL);
+    printf("Package get from the client!!! \n");
+
+    // Send the message
+    char server_message[255] = "Welcome to Minesweeper in C! ";
+    send(client_socket, server_message, sizeof(server_message), 0);
+
+    printf("Close the Socket \n");
+    
+    // Close the socket
+    close(server_socket);
+}
 
 // initialising game
 

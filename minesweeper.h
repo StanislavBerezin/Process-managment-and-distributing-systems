161fbadefc9a724ea7d04a41ad1450967549b408
Eric Lee <Raidenlkl@gmail.com>
2018-10-29 20:48:37 +1000

    header comment added

--- minesweeper.h
+++ minesweeper.h
@@ -23,30 +23,44 @@ int mainGame();
 
 void cleanGame();
 
-void find_adjacent();
-
-void update_adj_count(int i, int j);
-
-int cal_adjacent_mines(int x, int y);
-
-char *print_game();
-
-char *print_fullGameBroad();
-
-void place_mines();
-
+// A function to check this coordinate is mine or not
 int tile_contains_mine(int x, int y);
 
-void placeFlag(char tileA, char tile1);
+// A function to place the mines at random position
+void place_mines();
 
-void revealTile(char tileA, char tile1);
+// A function that generate a string that can display the full game Broad (include all the information for all coordinate)
+char *print_fullGameBroad();
 
+// A function that create string to display a game broad to the user (according to the current game status)
+char *print_game();
+
+// A function to calculate the adj mines faster, if this position is mines then update the grid adj mines number 
+void update_adj_count(int i, int j);
+
+// A function that counting the adj number for this coordinate
+int cal_adjacent_mines(int x, int y);
+
+// a function that detect a mine and then update the adj tile count for the coordinate nearby
 void reveal_adj(int i, int j);
 
+// it is a function to support user reveal the zero tile, it will reveal the other nearby this zero tile
 void reveal_ZeroAdj(int x, int y);
 
+// a function that detect a mine and then update the adj tile count for the coordinate nearby
+void find_adjacent();
+
+// A function to decide the decision what should do if the user reveal the tile
+void revealTile(char tileA, char tile1);
+
+// A function that place a flag to the position
+void placeFlag(char tileA, char tile1);
+
+//Check the user , is it won the game
 int getResult();
 
+// If the user lose return the time to the user
 int getStatus();
 
+//  Return the time to the user
 int getTime();
